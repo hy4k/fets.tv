efd511ca356a40cb8b673f9915b548468d375edc
hy4k <mithun@fets.in>
2026-09-15 01:38:38 +0530

    Add app/setup/page.tsx

---
+++ app/setup/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from '@/app/layout';
+import { SetupSettings } from '@/components/setup-settings';
+export default function SetupPage() { return <AppShell active="setup"><SetupSettings /></AppShell>; }
