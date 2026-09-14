4e201f022f542397c7ba2c2be5f6cdca68ec485b
hy4k <mithun@fets.in>
2026-09-15 01:38:29 +0530

    Add app/front/page.tsx

---
+++ app/front/page.tsx
@@ -0,0 +1,3 @@
+import { AppShell } from '@/app/layout';
+import { FrontOffice } from '@/components/front-office';
+export default function FrontPage() { return <AppShell active="front"><FrontOffice /></AppShell>; }
